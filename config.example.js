f89c55288654085d36ff696cb457270c66cbaa01
Mark Dietzer <mriq91@gmail.com>
2016-07-14 05:09:57 +0200

    Initial commit

---
+++ config.example.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+	channels: {
+		normal: '202948351027707905',
+		staff: '202948384422756353'
+	},
+	guildId: '93446384836939776',
+	botToken: '[REDACTED]',
+	roles: {
+		member: '202947245417431040',
+		regular: '202947369308782593',
+		trusted: '202947456537722880',
+		admin: '202947498342350848',
+		superadmin: '202947541598339072',
+		doridian: '202947572090929152',
+	},
+	redis: {
+		host: '127.0.0.1',
+		port: 6379,
+		db: 1,
+		password: '[REDACTED]',
+	},
+};
---
+++ index.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const config = require('./config');
+const Promise = require('bluebird');
+const Discord = require('discord.js');
+const redis = Promise.promisifyAll(require('redis')
+.createClient(config.redis.port, config.redis.host, {
+    password: config.redis.password,
+    db: config.redis.db,
+}));
+const bot = Promise.promisifyAll(new Discord.Client());
+
+const ROLE_MAPPING = config.roles;
+
+const MANAGED_ROLES = {};
+for (const i in ROLE_MAPPING) {
+	MANAGED_ROLES[ROLE_MAPPING[i]] = true;
+}
+
+function getMCUUIDForUser (user) {
+	return redis.hgetAsync('discordlinks:discord-to-mc', user.id);
+}
+
+function getMCRoleForUser (user) {
+	return getMCUUIDForUser(user)
+	.then(uuid => {
+		console.log(uuid);
+		if (uuid) {
+			return redis.hgetAsync('playergroups', uuid);
+		}
+		return null;
+	})
+	.then(role => {
+		console.log(role);
+		if (role && ROLE_MAPPING[role]) {
+			return role;
+		}
+		return null;
+	});
+}
+
+function syncMCRolesForUser (user) {
+	return getMCRoleForUser(user)
+	.then(redisMCRole => {
+		const correctDiscordRole = ROLE_MAPPING[redisMCRole];
+		const details = bot.servers[0].detailsOfUser(user);
+
+		const rolesToRemove = [];
+		let hasCorrectRole = false;
+		details.roles.forEach(role => {
+			if (role.id === correctDiscordRole) {
+				hasCorrectRole = true;
+			} else if(MANAGED_ROLES[role.id]) {
+				rolesToRemove.push(role.id);
+			}
+		});
+
+		let promise = Promise.resolve();
+
+		if (!hasCorrectRole && correctDiscordRole) {
+			promise = promise.then(() =>
+				bot.addMemberToRoleAsync(user, correctDiscordRole));
+		}
+
+		if (rolesToRemove.length > 0) {
+			promise = promise.then(() =>
+				bot.removeMemberFromRoleAsync(user, rolesToRemove));
+		}
+
+		return promise;
+	});
+}
+
+bot.on('ready', (data) => {
+	
+});
+
+bot.on('message', message => {
+	let translatedMessage = null;
+	if (message.channel.id === config.channels.normal) {
+		translatedMessage = message.content;
+	} else if(message.channel.id === config.channels.staff) {
+		translatedMessage = '/opchat ' + message.content;
+	}
+
+	if (!translatedMessage) {
+		return;
+	}
+
+	syncMCRolesForUser(message.author)
+	.then(() => bot.deleteMessageAsync(message));
+});
+
+bot.loginWithToken(config.botToken);
